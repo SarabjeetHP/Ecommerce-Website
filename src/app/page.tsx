8de636c59c671b2022e194f7f5fc9281d3d6f132
Sarabjeet Singh <iamsarabjeetsingh3@gmail.com>
2024-08-27 15:12:02 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello</h1>
       <h1>Ecommerce </h1>
-      <h3>Hell Aditi and sarab</h3>
+      <Footer />
     </main>
   );
 }
