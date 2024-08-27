45875973b124393bc041558d6b52c3e1fbb01c93
Sarabjeet Singh <iamsarabjeetsingh3@gmail.com>
2024-08-27 15:27:18 +0530

    CAPS

--- src/components/shared/Testimonial/Testimonial.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Testimonial = () => {
-  return <div>hiii</div>;
-};
-
-export default Testimonial;
--- src/components/shared/footer/footer.tsx
+++
@@ -1,4 +0,0 @@
-export default function footer() {
-  <h1>Footer</h1>;
-  <h2>Testing Github</h2>;
-}
