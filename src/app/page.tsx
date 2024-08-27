2c7ac0ad9eb943ebb058c29116ea25d58a648301
Yashavant Singh <yashavantsingh01.hp@outlook.com>
2024-08-27 15:04:33 +0530

    add some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<<<<<<< HEAD
       <h1>Hello</h1>
-=======
       <h1>Ecommerce </h1>
->>>>>>> 2ec39df123e92211f2e895b38a97f4d39d5a9ec8
+      <h3>Hell Aditi and sarab</h3>
     </main>
   );
 }
