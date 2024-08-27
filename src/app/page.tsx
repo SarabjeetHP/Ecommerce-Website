738298a6fc7133fa7f48c3762183d4fdb166ee6d
Yashavant Singh <yashavantsingh01.hp@outlook.com>
2024-08-27 15:54:43 +0530

    add some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import Footer from "@/components/shared/footer/footer";
+import Header from "@/Component/Shared/Header/Header";
+
+import Footer from "@/Component/Shared/Footer/Footer";
+
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Ecommerce </h1>
-      <Footer />
+      <Header/>
+      <Footer/>
+      
     </main>
   );
 }
