ef09cd52d85aba4bb427364d189201b9d597b857
Aditi-Shukla17 <shuklaaditi129@gmail.com>
2024-08-27 15:04:00 +0530

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<<<<<<< HEAD
       <h1>Hello</h1>
-=======
       <h1>Ecommerce </h1>
->>>>>>> 2ec39df123e92211f2e895b38a97f4d39d5a9ec8
     </main>
   );
 }
